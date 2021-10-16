38dfea53615147f6468cf2954646a8812b230e8f
ÐSergey Dziadevich <developersiteweb@gmail.com>
2021-10-16 16:57:34 +0300

    added edit roles component

---
+++ client/src/app/_services/admin.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsersWithRoles(): Observable<Partial<User[]>> {
+    return this.http.get<Partial<User[]>>(this.baseUrl + 'admin/users-with-roles');
+  }
+}
