bc42d507ec85bb101fad2d8f3f81a2a190abb52c
ÐSergey Dziadevich <developersiteweb@gmail.com>
2021-12-11 16:24:11 +0300

    added confirmation service

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanDeactivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../member/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean {
+  constructor(private confirmService: ConfirmService) {
+
+  }
+
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
     if (component.editForm.dirty) {
-      return confirm('Are you sure want to continue? Any unsaved changes will be lost');
+      return this.confirmService.confirm();
     }
     return true;
   }
